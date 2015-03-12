3e9c36cd9bb87a18a6d9c7839090f9d11d342bcf
Amr Hassan <amr.hassan@gmail.com>
2015-03-12 12:31:14 +0200

    Created a setup.py script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(name='openwrt-remote-manager',
+      version='1.0',
+      description='A Python module for remotely managing an OpenWRT instance',
+      author='Amr Hassan',
+      author_email='amr.hassan@gmail.com',
+      url='https://github.com/Jumpscale/openwrt-remote-manager',
+      packages=find_packages(),
+      )
